d3e35fbc9bd0f534844f9246d0f0cfc2c072a1df
Artem <a.khimin@yandex.ru>
2021-12-15 19:25:35 +0300

    Сделал программу генерации сигнатуры указанного файла вычисляющую значение hash-функции SHA256

--- ProjectSignature/Program.cs
+++ ProjectSignature/Program.cs
@@ -1,2 +1,22 @@
 ﻿
-Console.WriteLine("Hello, World!");
+using ProjectSignature;
+
+if (args.Length > 0)
+{
+    Console.WriteLine("Yes args!!!");
+    foreach (var i in args)
+    {
+        Console.WriteLine(i);
+    }
+    string path = args[0];                   // @"D:\Big\BigFile2.txt"; ~ 70Gb
+    string partSize = args[1];               //"1000";
+
+    InputFile file = new InputFile(path, partSize);
+
+    file.Run();
+}
+else
+{
+    Console.WriteLine("No args!!!");
+}
+Console.ReadLine();
